703500f6a064f7ed3ffbd6ba12c04eef2e484836
mamonu <thorosm@msn.com>
2016-10-26 14:56:37 +0100

    moved into R dir

---
+++ Rscripts/weight.R
@@ -0,0 +1,27 @@
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+
+
+#delete everything... start with a clean slate:
+rm(list=ls())
+
+setwd("~/scripts/HackTober")
+
+df<-read.csv("WeightLoss.csv",header = T,stringsAsFactors = F)
+
+
+summary(aov(formula = df$wl1 ~ df$group))
+
+ggplot(df, aes(x = group, y = wl1)) +
+  geom_boxplot(fill = "grey80", colour = "blue") +
+  scale_x_discrete() + xlab("Treatment Group") +
+  ylab("weight loss")
+
+
+
+TukeyHSD(aov(formula = df$wl1 ~ df$group))
+
+
+
+
