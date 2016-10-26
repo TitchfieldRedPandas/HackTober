38679954b87c5d6736675a55b84fff1ec596cd2c
mamonu <thorosm@msn.com>
2016-10-26 14:19:35 +0100

    1-way ANOVA added + viz

--- weight.R
+++ weight.R
@@ -1,5 +1,6 @@
 library(dplyr)
 library(tidyr)
+library(ggplot2)
 
 
 #delete everything... start with a clean slate:
@@ -11,3 +12,8 @@ df<-read.csv("WeightLoss.csv",header = T,stringsAsFactors = F)
 
 
 summary(aov(formula = df$wl1 ~ df$group))
+
+ggplot(df, aes(x = group, y = wl1)) +
+  geom_boxplot(fill = "grey80", colour = "blue") +
+  scale_x_discrete() + xlab("Treatment Group") +
+  ylab("weight loss")
